927450cce4b3822e250d5b4a2be69ea7321d4a0b
Fabio Di Gregorio <digre01@gmail.com>
2024-01-15 09:49:14 +0100

    Creazione della classe Knn e della funzione di training

--- M_Development.py
+++ M_Development.py
@@ -1,11 +1,21 @@
 import numpy as np # import della libreria numpy 
+from input import Input
 
-# Calcolo della distanza euclidea tramite una funzione
-def distanza_euclidea(x_1, x_2):
-    '''
-    La distanza si calcola come la radice della somma dei quadrati delle differenze 
-    La funzione restitursce la distanza calcolata 
-    '''
-    # Il calcolo avviene tramite l'ausilio della libreria numpy
-    dist = np.sqrt(np.sum((x_1-x_2)**2)) 
-    return dist 
+class Knn:
+    def __init__(self,k):
+        self.k = k
+
+    def training(self, X, Y):
+        self.X_train = X
+        self.Y_train = Y
+    
+    # Calcolo della distanza euclidea tramite una funzione
+    def distanza_euclidea(self,x_1, x_2):
+        '''
+        La distanza si calcola come la radice della somma dei quadrati delle differenze 
+        La funzione restitursce la distanza calcolata 
+        '''
+        # Il calcolo avviene tramite l'ausilio della libreria numpy
+        dist = np.sqrt(np.sum((x_1-x_2)**2)) 
+        return dist 
+
