83a905a9e69b01bd641e551e444bf0352e41fb23
Sebastiano Viglialoro <sebastianoviglialoro@MacBook-Air-di-Sebastiano.local>
2024-01-31 16:49:25 +0100

    Creazione del metodo ReaderCSV

--- ReaderCSV.py
+++ ReaderCSV.py
@@ -1,9 +1,10 @@
 import pandas as pd
-
+from ReaderFactory import Reader
 
 class ReaderCSV(Reader):
+
+    def parse(self, filename):
+        
+        df = pd.read_csv(filename)
+        return df
     
-
-
-
-
