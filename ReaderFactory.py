81cb2fc492a45f64e97cc4ee702dad87b643fb59
Sebastiano Viglialoro <sebastianoviglialoro@MacBook-Air-di-Sebastiano.local>
2024-01-31 17:05:08 +0100

    Creazione del metodo create_reader

--- ReaderFactory.py
+++ ReaderFactory.py
@@ -1,3 +1,10 @@
+from ReaderCSV import ReaderCSV
 
 class ReaderFactory:
-  
+
+    @staticmethod
+    def create_reader(filename):
+      
+        if filename.endswith('.csv'):
+            return ReaderCSV()
+        
