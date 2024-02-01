32de54adf875b64356052275f028f504197fe620
Fabio Di Gregorio <digre01@gmail.com>
2024-02-01 16:01:08 +0100

    Aggiunta dei commenti alla classe DataSplitter

--- F&L.py
+++ F&L.py
@@ -3,12 +3,17 @@ from ReaderFactory import readerfactory
 
 class DataSplitter:
     def __init__(self, df, label_column_name):
+        # Inizializza gli attributi della classe con i valori forniti
         self.df = df
         self.label_column_name = label_column_name
- 
+
+    # Metodo per suddividere il dataframe in features (caratteristiche) e label
     def split(self):
+        # Estrai le features dal dataframe escludendo la colonna delle label
         features = self.df.drop(self.label_column_name, axis=1)
+        # Estrai le label dal dataframe utilizzando solo la colonna delle label
         labels = self.df[self.label_column_name]
+        # Restituisci le features e le label come risultato della suddivisione
         return features, labels
 
 
