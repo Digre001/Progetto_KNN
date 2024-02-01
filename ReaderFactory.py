31c87db7af32c2f2e1b474cdaeed9e4b11e80689
Sebastiano Viglialoro <sebastianoviglialoro@MacBook-Air-di-Sebastiano.local>
2024-02-01 11:10:46 +0100

    Aggiunta dei commenti nel file ReaderFactory.py

--- ReaderFactory.py
+++ ReaderFactory.py
@@ -1,10 +1,16 @@
 from ReaderCSV import ReaderCSV
-
+# classe factori per creare oggetti reader in base all'estensione del file 
 class ReaderFactory:
 
-    @staticmethod
+    @staticmethod # utilizo del metodo statico 
     def create_reader(filename):
+        '''
+        questo metodo crea e restituisce un oggeto reade in base all'estensione del file.
+        Se l'estensione del file .csv, crea un ReaderCSV
+        Se l'estensione del file non è supportata, solleva un 'eccezione Valie Error
+        '''
       
         if filename.endswith('.csv'):
             return ReaderCSV()
+        #aggiungere un valureerror?
         
