e61b242d196243165263020cfeccf243f3955804
Sebastiano Viglialoro <sebastianoviglialoro@MacBook-Air-di-Sebastiano.local>
2024-02-01 11:14:23 +0100

    Aggiunta dei commenti al file ReaderCSV

--- ReaderCSV.py
+++ ReaderCSV.py
@@ -2,8 +2,16 @@ import pandas as pd
 from ReaderFactory import Reader
 
 class ReaderCSV(Reader):
+    '''
+    Implementazione della classe concreta ReaderCSV, una classe figlia della clsasse astratta Reader.
+    il suo unico compito è leggere file CSV, trasformali in Dataframe e restituirli pronti per il preprocessing.
+    '''
 
     def parse(self, filename):
+        '''
+        Implementaizone concreda del metodo "prase" che riceve in input il nume del file CSV da leggere 
+        e restituisce un Dataframe pronto per il pre-processing
+        '''
         
         df = pd.read_csv(filename)
         return df
