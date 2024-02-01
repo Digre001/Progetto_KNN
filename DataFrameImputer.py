cea5aa48a97ccc7fcc7ae54ab415a1104dbbecad
Sebastiano Viglialoro <sebastianoviglialoro@MacBook-Air-di-Sebastiano.local>
2024-02-01 11:17:28 +0100

    Aggiunta dei commenti nel file DataFrameImputer

--- DataFrameImputer.py
+++ DataFrameImputer.py
@@ -1,7 +1,13 @@
 
 class DataframeImputer:
+    '''
+    Classe dataframeimputer che ha il compito di riempire i valori mancanti in un dataframe 
+    '''
 
     def impute_missing_values(self, df):
+        '''
+        Metodo per riempire i valori mancanti di un dataframe con la moda della colonna corripondente
+        '''
         
         for col in df.columns:
             if df[col].isnull().any():
