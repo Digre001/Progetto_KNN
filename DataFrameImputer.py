e81ada1a9314090b04c9471e56af09eb0507de8d
Sebastiano Viglialoro <sebastianoviglialoro@MacBook-Air-di-Sebastiano.local>
2024-02-01 10:16:11 +0100

    Aggiunta del metodo impute_missing_values nella classe DataframerImputer

--- DataFrameImputer.py
+++ DataFrameImputer.py
@@ -1,4 +1,12 @@
 
 class DataframeImputer:
-   
+
+    def impute_missing_values(self, df):
+        
+        for col in df.columns:
+            if df[col].isnull().any():
+                mode_value = df[col].mode()[0] 
+                df[col].fillna(mode_value, inplace=True)
+
+        return df
 
