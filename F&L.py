d8ea346c9c79fb78e40a49e683aeaeabfd4b83a5
Fabio Di Gregorio <digre01@gmail.com>
2024-02-01 22:30:27 +0100

    Aggiunta del metodo process_data nella classe DataSplitter

--- F&L.py
+++ F&L.py
@@ -1,19 +1,26 @@
 from DFInputer import dfInputer
 from ReaderFactory import readerfactory
+from input import Input
 
 class DataSplitter:
-    def __init__(self, df, label_column_name):
+    def __init__(self, file_name):
         # Inizializza gli attributi della classe con i valori forniti
-        self.df = df
-        self.label_column_name = label_column_name
+        self.file_name = file_name
+        self.input = Input()
 
     # Metodo per suddividere il dataframe in features (caratteristiche) e label
-    def split(self):
+    def split(self, df, label_column_name):
         # Estrai le features dal dataframe escludendo la colonna delle label
-        features = self.df.drop(self.label_column_name, axis=1)
+        features = df.drop(label_column_name, axis=1)
         # Estrai le label dal dataframe utilizzando solo la colonna delle label
-        labels = self.df[self.label_column_name]
+        labels = df[label_column_name]
         # Restituisci le features e le label come risultato della suddivisione
         return features, labels
-
-
+    
+    def process_data(self):
+        reader = readerfactory.create_reader(self.file_name)
+        df = reader.parse(self.file_name)
+        method = self.input. # nome della variabile da chiamare che ora non ricordo 
+        df_ready = dfInputer(df).handle_missing_values(method)
+        nome = df_ready.columns[-1]
+        return self.split(df_ready, nome)
