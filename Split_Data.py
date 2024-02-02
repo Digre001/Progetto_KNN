9d8e5b1c1356ce9d6c165754676ad3ec5890d94c
Lorenzo-Pagliaricci <lorenzopagliaricci@gmail.com>
2024-02-02 17:56:18 +0100

    Aggiunto commenti del costruttore e del metodo SplitHoldout
    ho aggiutno i commenti per i due metodi e aggiornato un po come ho scritto il metodo SplitHoldout

--- Split_Data.py
+++ Split_Data.py
@@ -2,7 +2,13 @@ import pandas as pd
 
 
 class Split_data:
-
+    '''
+        costruttore:
+        features: sono i dati che verranno usati per addestrare e testare il modello
+        target: sono i dati che utilizzeremo per verificare il training e per verificare il test
+        train_size: percentuale di dati che verranno utilizzati per il metodo di holdout
+        N_esperimenti: numero di esperimenti da fare per il leavePout corss validation
+        '''
     def __init__(self, features: pd.DataFrame, target: pd.Series, train_size: int, N_esperimenti: int, k):
         self.features = features
         self.target = target
@@ -10,18 +16,26 @@ class Split_data:
         self.N_esperimenti=N_esperimenti
 
 
+    '''
+        I valori che mi servono e che quindi voglio riutilizzare sono questi:
+        (tramite return li inserisco nella variabile Split_Holdout)
+
+        X_train: dati per addestrare il modello
+        Y_train: dati per addestrare il modello (risultato che conosco)
+        x_test: dati per tetare il modello
+        y_test: dati per testare il modello finale confrpntandolo con le uscite che mi restituisce il mio modello creato
+        '''
 
     def Split_Holdout(self, features: pd.DataFrame, target: pd.Series, train_size: int):
-
         # Prendo i dati per il training secondo una percentuale specificata in input
-        X_train = features.sample(frac=train_size)
-
+        X_train = self.features.sample(frac=self.train_size)
         # Prendo i dati per il test. Sono tutti i dati che non sono stati presi per il training
-        x_test = features.drop(X_train.index)
+        x_test = self.features.drop(X_train.index)
 
-        Y_train = target.drop(x_test.index)  # Mi salvo le y di train corrispondenti alle x di train
-
-        y_test = target.drop(X_train.index)  # Mi salvo le y di test corrispondenti alle x di test
+        # Mi salvo le y di train corrispondenti alle x di train
+        Y_train = self.target.drop(x_test.index)
+        # Mi salvo le y di test corrispondenti alle x di test
+        y_test = self.target.drop(X_train.index)
 
         return X_train, Y_train, x_test, y_test
 
