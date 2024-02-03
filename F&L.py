aa766f28a314fee71f39b3aa74133fae2ce1ba4c
Fabio Di Gregorio <digre01@gmail.com>
2024-02-03 17:02:26 +0100

    Preprocessing pronto per i test

--- F&L.py
+++ F&L.py
@@ -1,12 +1,10 @@
 from DFInputer import dfInputer
 from ReaderFactory import readerfactory
-from input import Input
 
 class DataSplitter:
     def __init__(self, file_name):
         # Inizializza gli attributi della classe con i valori forniti
         self.file_name = file_name
-        self.input = Input()
 
     # Metodo per suddividere il dataframe in features (caratteristiche) e label
     def split(self, df, label_column_name):
@@ -18,15 +16,20 @@ class DataSplitter:
         return features, labels
     
     def process_data(self):
-        # richiamo della metodo create_readr della classe readerfactory
+        # richiamo della metodo create_reader della classe readerfactory
         reader = readerfactory.create_reader(self.file_name)
         # Utilizza reader per analizzare il file e ottenere un dataframe
         df = reader.parse(self.file_name)
-        # Ottiene il metodo per gestire i valori mancanti dal file di input
-        method = self.input.Gestione
         # Crea un'istanza di dfInputer con il dataframe ottenuto e utilizza il metodo specificato per gestire i valori mancanti
-        df_ready = dfInputer(df).handle_missing_values(method)
+        df_ready = dfInputer(df).handle_missing_values("mean")  # Puoi scegliere tra drop, mean, median, mode
         # Ottiene il nome dell'ultima colonna del dataframe
         nome = df_ready.columns[-1]
         # Chiama il metodo split per suddividere il dataframe in features e labels utilizzando il nome ottenuto come colonna delle label
         return self.split(df_ready, nome)
+
+# Aggiunte le seguenti righe alla fine del codice
+a = DataSplitter('test.csv')
+features, labels = a.process_data()
+print("Features:\n", features)
+print("Labels:\n", labels)
+
