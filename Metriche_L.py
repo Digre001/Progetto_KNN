7c10068ab1088e061f5ae962d0a208a605c3da5a
Lorenzo-Pagliaricci <lorenzopagliaricci@gmail.com>
2024-02-05 11:25:02 +0100

    aggiorno e implemento classe metriche
    finito di fare il calcolo della confusiuon matrix e delle metriche

--- Metriche_L.py
+++ Metriche_L.py
@@ -1,25 +1,50 @@
 import pandas as pd
+import numpy as np
 
 class Metriche:
-    
+
     def __init__(self,y_test: pd.Series,previsioni: list):
         self.y_test=y_test
         self.previsioni=previsioni
 
-    def calcolo_metriche(self):
+    def calcolo_matrix_metriche(self):
         # calcolo la confusion Matrix:
 
         # Calcolo il valore True Negative della confusion matrix
-        true_negative = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y == pred and pred == 2))
+        vero_negativo = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y == pred and pred == 2))
+        # (zip mi serve per combinare gli elementi y_test e conforntarli con le previsioni e lìif quindi mi fa il conteggio
+        # in questo caso solo se y e pred coincidono con 2, quindi con 1 for y, ecc.. if ecc.. sto dicendo che mi genere un
+        # 1 ogni qualvolta la condizione if è soddisfatta e poi con sum sommo tutti questi uni).
 
         # Calcolo il valore True Positive della confusion matrix
-        true_positive = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y == pred and pred == 4))
+        vero_positivo = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y == pred and pred == 4))
 
         # Calcolo il valore False Positive della confusion matrix
-        false_positive = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y != pred and pred == 2))
+        falso_positivo = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y != pred and pred == 2))
 
         # Calcolo il valore False Negative della confusion matrix
-        false_negative = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y != pred and pred == 4))
+        falso_negativo = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y != pred and pred == 4))
+
+        '''Calcolo effettivo delle metriche richieste mediante i valori della confusion matrix, precedentemente calcolati
+                '''
+        # Percentuale di predizioni corrette rispetto al totale delle predizioni
+        Accuracy_rate = (vero_negativo + vero_positivo) / self.y_test.size
+
+        # Percentuale di predizioni errate rispetto al totale delle predizioni
+        Error_rate = (falso_positivo + falso_negativo) / self.y_test.size
+
+        # Capacità del modello di predire correttamente i valori positivi
+        Sensitivity = (vero_positivo) / (vero_positivo + falso_negativo)
+
+        # Capacità del modello di predire correttamente i valori negativi
+        Specificity = (vero_negativo) / (vero_negativo + falso_positivo)
+
+        # Media che bilancia valori positivi e negativi
+        Geometric_mean = np.sqrt((Sensitivity * Specificity))
+
+        return Accuracy_rate, Error_rate, Sensitivity, Specificity, Geometric_mean
+
+
 
     def salvare_metriche(self):
         pass
