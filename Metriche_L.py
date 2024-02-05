7a889cdc372b780f683e0caab318af6152b3ed43
Lorenzo-Pagliaricci <lorenzopagliaricci@gmail.com>
2024-02-05 11:19:19 +0100

    aggiorno classe metriche
    aggiunto il costruttore nella classe, implementato il calcolo metriche

--- Metriche_L.py
+++ Metriche_L.py
@@ -1,7 +1,25 @@
+import pandas as pd
+
 class Metriche:
+    
+    def __init__(self,y_test: pd.Series,previsioni: list):
+        self.y_test=y_test
+        self.previsioni=previsioni
 
     def calcolo_metriche(self):
-        pass
+        # calcolo la confusion Matrix:
+
+        # Calcolo il valore True Negative della confusion matrix
+        true_negative = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y == pred and pred == 2))
+
+        # Calcolo il valore True Positive della confusion matrix
+        true_positive = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y == pred and pred == 4))
+
+        # Calcolo il valore False Positive della confusion matrix
+        false_positive = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y != pred and pred == 2))
+
+        # Calcolo il valore False Negative della confusion matrix
+        false_negative = sum(1 for y, pred in zip(self.y_test, self.previsioni) if (y != pred and pred == 4))
 
     def salvare_metriche(self):
         pass
